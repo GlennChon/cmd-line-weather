0c5886b606c3c1e0bacf02c4114ed3515423816d
Glennch <glenn.chon@gmail.com>
2020-06-25 15:09:17 -0400

    api calls and response storage

--- weather.js
+++ weather.js
@@ -4,11 +4,65 @@ const moment = require('moment')
 const fetch = require('node-fetch')
 const api = require('./api')
 
-const getWeather = async () => {}
+const getWeather = async () => {
+  let inputValue = process.argv.slice(2)
+  let requests = inputValue.map(async (val) => {
+    const queryType = setQueryType(val)
+    const urlVal = encodeURI(val)
+    const url = `${api.url}${queryType}=${urlVal}&appid=${api.key}`
+    return await getResp(url, val)
+  })
 
-const setQueryType = (val) => {}
+  return Promise.allSettled(requests).then((responses) => {
+    return responses.map((resp) => {
+      return resp.value
+    })
+  })
+}
 
-const getResp = async (url, val) => {}
+const setQueryType = (val) => {
+  if (val === undefined) {
+  } else if (!isNaN(val)) {
+    return 'zip'
+  } else {
+    return 'q'
+  }
+}
+
+const getResp = async (url, val) => {
+  return await fetch(url)
+    .then((res) => {
+      if (res.status !== 200) {
+        throw Error(`${res.statusText} for ${val}`)
+      } else {
+        return res.json()
+      }
+    })
+    .then((json) => {
+      if (json !== undefined) {
+        return {
+          searchTerm: val,
+          city: json.name,
+          weather: json.weather[0].main,
+          description: json.weather[0].description,
+          time: getLocalTime(json.timezone),
+          tempC: kelvinToC(json.main.temp),
+          tempF: kelvinToF(json.main.temp)
+        }
+      }
+    })
+    .catch((error) => {
+      return {
+        searchTerm: val,
+        city: 'N/A',
+        weather: 'N/A',
+        description: error.message,
+        time: 'N/A',
+        tempC: 'N/A',
+        tempF: 'N/A'
+      }
+    })
+}
 
 const kelvinToF = (k) => (k * 1.8 - 459.67).toFixed(2)
 
@@ -17,7 +71,10 @@ const kelvinToC = (k) => (k - 273.15).toFixed(2)
 const getLocalTime = (UTCSecOffset) =>
   new moment().utc().add(UTCSecOffset, 's').format('HH[:]mm')
 
-const displayWeather = async () => {}
+const displayWeather = async () => {
+  let allLocations = await getWeather()
+  console.log(allLocations)
+}
 
 displayWeather()
 module.exports = {
