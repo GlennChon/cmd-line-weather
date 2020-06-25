7059ec3ed4f950652e66681fb5d12d87353ff3be
Glennch <glenn.chon@gmail.com>
2020-06-25 15:35:25 -0400

    test setup

---
+++ weather.test..js
@@ -0,0 +1,17 @@
+// test file
+
+describe('Setup', () => {
+  it("Should set url type query: 'q' ", () => {
+    // setQueryType,
+  })
+})
+
+describe('API Call/Response', () => {
+  // getWeather,
+  // getResp,
+})
+describe('Conversions', () => {
+  // getLocalTime,
+  // kelvinToF,
+  // kelvinToC
+})
